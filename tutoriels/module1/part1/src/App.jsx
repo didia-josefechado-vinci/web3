8d0c5d5fce7cb530fa4411b7a93b65a086f3374d
Didia <didia.josefechado@student.vinci.be>
2024-09-17 15:38:48 +0200

    refactor

--- tutoriels/module1/part1/src/App.jsx
+++ tutoriels/module1/part1/src/App.jsx
@@ -1,25 +1,5 @@
 import './App.css'
 
-const Hello = (props) => {
-
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
-
 const App = () => {
   const friends = [ 'Peter', 'Maya']
 
