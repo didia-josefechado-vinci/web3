17fad9e91c7319bbdf55e35f611dca2080cd6d35
Didia <didia.josefechado@student.vinci.be>
2024-10-12 16:42:07 +0200

    unicaf 1.9

--- exercises/module3/src/Components/App/App.jsx
+++ exercises/module3/src/Components/App/App.jsx
@@ -1,30 +1,41 @@
 import { useState } from 'react'
 
+
+const Statistics = (props) => {
+    if (props.good + props.neutral + props.bad === 0) {
+        return (
+            <div>
+                <p>No feedback given</p>
+            </div>
+        )
+    }else{
+        return (
+        <div>
+            <p>good {props.good}</p>
+            <p>neutral {props.neutral}</p>
+            <p>bad {props.bad}</p>
+            <p>all {props.good + props.neutral + props.bad}</p>
+            <p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
+            <p>positive {(props.good / (props.good + props.neutral + props.bad)) * 100} %</p>
+        </div>
+        )
+    }
+  }
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = (good / all) * 100
-
 
   return (
     <div>
-        <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
-
 export default App
